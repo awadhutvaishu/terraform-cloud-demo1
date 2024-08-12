3799a2460f08ece194464bd2425b1a1ff695ec9f
awadhutvaishu <awadhutvaishnavi23@gmail.com>
2024-08-12 18:46:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+  }
+  data "aws_ami" "myami"{
+  most_recent= true
+  owners = ["amezon"]
+  filter {
+  name = "name"
+  values = ["amzn2-ami-hvm*"]
+  }
+  }
+  resource "aws_instance" "myec2" {
+  ami      =data.aws_ami.myami.id
+  instance_type="t2.micro"
+  tags= {
+  name = "instance-1"
+  }
+   cunt = 1
+   }
